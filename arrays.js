ac167f823d9fcf062aebeb65aa44412f88103cc2
Ofelia <oer.rodriguez@gmail.com>
2021-03-21 01:13:29 +0000

    Translated arrays, functions and objects from python to js

--- arrays.js
+++ arrays.js
@@ -3,17 +3,40 @@
 
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
+ for (const i in items) {
+  console.log(items[i], i);
+ }
 }
-
+ 
+// console.log(printIndices(['apple', 'banana', 'cherry']))
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  const result = [];
+
+  for (const i in items) {
+    if (i % 2 === 0) {
+      result.push(items[i])
+    }
+  }
+
+  return result
 }
 
+// console.log(everyOtherItem(['apple', 'banana', 'cherry']))
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+  //  """Print a list of the `n` smallest integers in `items`.
+  
+  const sorted_items = items.sort((a, b) => {
+    return a - b
+  });
+  const sorted_n_items = sorted_items.slice(0, n);
+  sorted_n_items.reverse();
+
+  console.log(sorted_n_items)
+
 }
+
+// console.log(smallestNItems([1, 30, 4, 21, 100000], 4))
--- functions.js
+++ functions.js
@@ -3,14 +3,38 @@
 
 // 1. isHometown
 
-// Define your function here
+function isHometown(town){
+    return town === 'San Francisco';
+}
 
+// console.log(isHometown('San Francisco'))
 
 // 2. getFullName
 
-// Define your function here
+function getFullName(first, last){
+    return `${first} ${last}`;
+}
 
+// console.log(getFullName("Barb", "Larry"))
 
 // 3. calculateTotal
 
-// Define your function here
+function calculateTotal(basePrice, state, tax=0.05) {
+    const subtotal = basePrice * (1 + tax);
+    let fee = 0;
+
+    if (state === "CA") {
+        fee = 0.03 * subtotal;
+    } else if (state === "PA") {
+        fee = 2;
+    } else if (state == "MA"){
+        if (basePrice <= 100) {
+            fee = 1;
+        } else {
+            fee = 3
+        }
+    }
+    return subtotal + fee
+}
+
+// console.log(calculateTotal(5, "CA"))
